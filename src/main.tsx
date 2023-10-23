dd0cc2e4548c0448823c5ca0fbbb3e0bce893c87
issac4892 <issac4892@gmail.com>
2023-10-23 14:00:36 +0900

    start

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,19 @@
-import ReactDOM from 'react-dom/client'
-import App from './pages/App.tsx'
-import './styles/index.css'
+import ReactDOM from 'react-dom/client';
+import App from './pages/App.tsx';
+import './styles/index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
+import Nologin from './pages/Nologin.tsx';
 
 const router = createBrowserRouter([
-  { path: '/', element: <App /> },
-])
+  {path: '/', element: <App/>},
+  {path: '/nologin', element: <Nologin/>},
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <RecoilRoot>
-    <RouterProvider router={router} />
-  </RecoilRoot>,
-)
+  <div className="maincontainer">
+    <RecoilRoot>
+      <RouterProvider router={ router }/>
+    </RecoilRoot>
+  </div>,
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, resolveBaseUrl } from 'vite';
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    //basicSsl(),
     VitePWA({
       registerType: 'autoUpdate',
       devOptions: {
