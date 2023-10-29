81560f77001d2a3e710debf884d811954b516bc1
issac4892 <issac4892@gmail.com>
2023-10-29 17:19:33 +0900

    add pwa icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, resolveBaseUrl } from 'vite';
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
-import basicSsl from '@vitejs/plugin-basic-ssl'
+//import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,27 +13,25 @@ export default defineConfig({
       devOptions: {
         enabled: true
       },
-      /*
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'safari-pinned-tab.svg'],
       manifest: {
         name: '모다',
         short_name: '모다',
-        description: '모다',
+        description: '버스의 가격, 택시의 편안함.',
         theme_color: '#ffffff',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: 'android-chrome-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           }
         ]
       }
-    */
     })
   ],
 })
